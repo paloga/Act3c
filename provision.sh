a8df067e6210dea4ad49c665ec41d8187366baa8
Pablo <pabete.l@gmail.com>
2017-12-11 09:46:41 +0100

    Subimos provision

--- provision.sh
+++ provision.sh
@@ -1,2 +1,7 @@
 apt-get update
 apt-get install -y apache2
+apt-get install -y git
+cd /vagrant/
+git clone https://github.com/paloga/sitio1
+sudo rm -rf /var/www/html/
+sudo ln -fs /vagrant/sitio1 /var/www/html
