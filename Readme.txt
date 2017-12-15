548351a59b03067030a4cae536de8c32d7412d46
Pablo <pabete.l@gmail.com>
2017-12-15 11:19:49 +0100

    Subimos la actividad 3c

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+En la carpeta compartida por defecto tienes que crear los 2 ficheros de 
+configuracion en este caso "sitio1.conf" y "sitio2.conf" en una carpta config por ejemplo.
+Y hacemos lo mismo en una carpeta html creamos dos directorios dentro de html.
+sitio1 y sitio2 y dentro ponemos los .html. Despues ejecutamos el script de provision.
+Y funcionaria todo correctamente si las rutas estan todas bien.
--- provision.sh
+++ provision.sh
@@ -1,7 +1,15 @@
 apt-get update
 apt-get install -y apache2
 apt-get install -y git
-cd /vagrant/
-git clone https://github.com/paloga/sitio1
-sudo rm -rf /var/www/html/
-sudo ln -fs /vagrant/sitio1 /var/www/html
+#cd /vagrant/
+#git clone https://github.com/paloga/sitio1
+#sudo rm -rf /var/www/html/
+#sudo ln -fs /vagrant/html /var/www/html
+#cd /
+sudo cp /vagrant/html/sitio1/index.html /var/www/html/sitio1/index.html
+sudo cp /vagrant/html/sitio2/index.html /var/www/html/sitio2/index.html
+sudo cp /vagrant/config/sitio1.conf /etc/apache2/sites-available/sitio1.conf
+sudo cp /vagrant/config/sitio2.conf /etc/apache2/sites-available/sitio2.conf
+sudo a2ensite sitio1.conf
+sudo a2ensite sitio2.conf
+sudo service apache2 restart
